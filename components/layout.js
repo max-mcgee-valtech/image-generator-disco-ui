335588e0994866f995d77f0f161251333957421e
max-mcgee-valtech <max.mcgee@valtech.com>
2022-05-10 11:40:27 -0700

    first commit

---
+++ components/layout.js
@@ -0,0 +1,41 @@
+export default function Layout({ children }) {
+  return (
+    <div>
+      {children}{" "}
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
+        </a>
+      </footer>
+      <style jsx>{`
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+      `}</style>
+    </div>
+  );
+}
