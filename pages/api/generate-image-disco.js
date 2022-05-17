ec5d8b26fc8de374bbefdc5df3e9081605c265cd
max-mcgee-valtech <max.mcgee@valtech.com>
2022-05-17 16:01:03 -0700

    updates db

--- pages/api/generate-image-disco.js
+++ pages/api/generate-image-disco.js
@@ -6,20 +6,20 @@ export default async function handler(req, res) {
   const textPrompt = req.body.textPrompt;
 
   const newId = uuid();
-  let prisma = new PrismaClient();
+  // let prisma = new PrismaClient();
 
   await cloudinary.v2.uploader.upload(
     "https://res.cloudinary.com/detzng4ks/image/upload/v1652744227/gray_rmkqbm.png",
     { public_id: newId, folder: "/disco-diffusion-active-tests" }
   );
 
-  await prisma.image.create({
-    data: {
-      textPrompt,
-      status: "pending",
-      imageId: newId,
-    },
-  });
+  // await prisma.image.create({
+  //   data: {
+  //     textPrompt,
+  //     status: "pending",
+  //     imageId: newId,
+  //   },
+  // });
 
   await fetch("http://13.56.154.163:5000/handle_data", {
     method: "POST",
