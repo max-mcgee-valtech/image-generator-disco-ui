2d32d684c8cf6c28403af0b477f10e06e02dd919
max-mcgee-valtech <max.mcgee@valtech.com>
2022-05-18 10:00:10 -0700

    adds api route

--- pages/api/generate-image-disco.js
+++ pages/api/generate-image-disco.js
@@ -15,19 +15,19 @@ export default async function handler(req, res) {
     }
   );
 
-  fetch("http://13.56.154.163:5000/handle_data", {
-    method: "POST",
-    headers: {
-      "Bypass-Tunnel-Reminder": "go",
-      mode: "no-cors",
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      textPrompt,
-      imageId: newId,
-    }),
-  });
+  // fetch("http://13.56.154.163:5000/handle_data", {
+  //   method: "POST",
+  //   headers: {
+  //     "Bypass-Tunnel-Reminder": "go",
+  //     mode: "no-cors",
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     textPrompt,
+  //     imageId: newId,
+  //   }),
+  // });
 
   res.status(200).send({
     currentPrompt: textPrompt,
