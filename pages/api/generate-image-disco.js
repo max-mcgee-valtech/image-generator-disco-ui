84601b10ba2ebc2d7a411cec2716691abbda6482
max-mcgee-valtech <max.mcgee@valtech.com>
2022-05-18 10:11:16 -0700

    adds api route

--- pages/api/generate-image-disco.js
+++ pages/api/generate-image-disco.js
@@ -5,15 +5,20 @@ export default async function handle(req, res) {
   const textPrompt = req.body.textPrompt;
   const status = req.body.status;
   const newId = uuid();
+  cloudinary.config({
+    cloud_name: "detzng4ks",
+    api_key: process.env.API_KEY_CLOUDINARY,
+    api_secret: process.env.CLOUDINARY_SECRET,
+  });
 
-  // await cloudinary.v2.uploader.upload(
-  //   "https://res.cloudinary.com/detzng4ks/image/upload/v1652744227/gray_rmkqbm.png",
-  //   {
-  //     public_id: newId,
-  //     folder: "/disco-diffusion-active-tests",
-  //     context: `alt=${status}|caption=${textPrompt}`,
-  //   }
-  // );
+  await cloudinary.v2.uploader.upload(
+    "https://res.cloudinary.com/detzng4ks/image/upload/v1652744227/gray_rmkqbm.png",
+    {
+      public_id: newId,
+      folder: "/disco-diffusion-active-tests",
+      context: `alt=${status}|caption=${textPrompt}`,
+    }
+  );
 
   fetch("http://13.56.154.163:5000/handle_data", {
     method: "POST",
