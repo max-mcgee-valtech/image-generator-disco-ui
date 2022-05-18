438483d421148045cb6545573d91c8a96a47a831
max-mcgee-valtech <max.mcgee@valtech.com>
2022-05-18 10:07:31 -0700

    adds api route

--- pages/api/generate-image-disco.js
+++ pages/api/generate-image-disco.js
@@ -6,27 +6,27 @@ export default async function handle(req, res) {
   const status = req.body.status;
   const newId = uuid();
 
-  await cloudinary.v2.uploader.upload(
-    "https://res.cloudinary.com/detzng4ks/image/upload/v1652744227/gray_rmkqbm.png",
-    {
-      public_id: newId,
-      folder: "/disco-diffusion-active-tests",
-      context: `alt=${status}|caption=${textPrompt}`,
-    }
-  );
+  // await cloudinary.v2.uploader.upload(
+  //   "https://res.cloudinary.com/detzng4ks/image/upload/v1652744227/gray_rmkqbm.png",
+  //   {
+  //     public_id: newId,
+  //     folder: "/disco-diffusion-active-tests",
+  //     context: `alt=${status}|caption=${textPrompt}`,
+  //   }
+  // );
 
-  // fetch("http://13.56.154.163:5000/handle_data", {
-  //   method: "POST",
-  //   headers: {
-  //     "Bypass-Tunnel-Reminder": "go",
-  //     mode: "no-cors",
-  //     Accept: "application/json",
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({
-  //     textPrompt,
-  //     imageId: newId,
-  //   }),
-  // });
+  fetch("http://13.56.154.163:5000/handle_data", {
+    method: "POST",
+    headers: {
+      "Bypass-Tunnel-Reminder": "go",
+      mode: "no-cors",
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      textPrompt,
+      imageId: newId,
+    }),
+  });
   return res.json({ currentPrompt: textPrompt });
 }
